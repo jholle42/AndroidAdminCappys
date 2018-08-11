b04d3aac801c16a559f7265c5c757dac665ce90f
jholle42 <jh522512@ohio.edu>
2018-08-11 11:53:49 -0400

    Functionality done , onto push notifications

--- app/src/main/java/jackal/org/adminandroid/RVAdapter.java
+++ app/src/main/java/jackal/org/adminandroid/RVAdapter.java
@@ -1,49 +1,20 @@
 package jackal.org.adminandroid;
 
-import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by jholle42 on 6/20/18.
+ * Created by jholle42 on 8/10/18.
  */
 
-public class RVAdapter extends RecyclerView.Adapter<RVAdapter.holdViewHolder>{
-
-    List<hold> holds;
-    FirebaseDatabase mFirebaseDatabase;
-
-    Context c;
-
-    DatabaseReference mHoldReference;
-
-
-    RVAdapter(List<hold> holds, DatabaseReference ref, Context c){
-        this.holds = holds;
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        this.mHoldReference = ref;
-        this.c = c;
-    }
-
-
-
-
-
-
-
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.holdViewHolder>  {
 
     public static class holdViewHolder extends RecyclerView.ViewHolder {
         RecyclerView rv;
@@ -51,7 +22,6 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.holdViewHolder>{
         TextView quantity;
         TextView item;
         TextView status;
-        Button deleteButton;
         holdViewHolder(View itemView) {
             super(itemView);
             rv = (RecyclerView)itemView.findViewById(R.id.sectionHolds);
@@ -59,8 +29,13 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.holdViewHolder>{
             quantity = (TextView)itemView.findViewById(R.id.holdQuantity);
             item = (TextView)itemView.findViewById(R.id.holdItem);
             status = (TextView)itemView.findViewById(R.id.holdStatus);
-            deleteButton = (Button)itemView.findViewById(R.id.holdDelete);
         }
+    }
+
+    List<hold> userHolds;
+
+    RVAdapter(List<hold> holds){
+        userHolds = holds;
     }
 
     @Override
@@ -71,90 +46,33 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.holdViewHolder>{
     }
 
     @Override
-    public int getItemCount() {
-        if(holds.equals(null)){
-            return holds.size();
-        }
-        return 0;
-    }
-
-    @SuppressLint("ResourceAsColor")
-    @Override
-    public void onBindViewHolder(final holdViewHolder personViewHolder,final int i) {
-        personViewHolder.name.setText(holds.get(i).name);
-        personViewHolder.quantity.setText(holds.get(i).quantity);
-        personViewHolder.item.setText(holds.get(i).itemName);
-        Integer stat = holds.get(i).getApproved();
+    public void onBindViewHolder(holdViewHolder ViewHolder, int i) {
+        ViewHolder.name.setText(userHolds.get(i).getName());
+        ViewHolder.quantity.setText(userHolds.get(i).getQuantity());
+        ViewHolder.item.setText(userHolds.get(i).getItemName());
+        Integer stat = userHolds.get(i).getApproved();
         if(stat.equals(1)){
-            personViewHolder.status.setBackgroundResource(R.color.colorGreen);
+            ViewHolder.status.setBackgroundResource(R.color.colorGreen);
         }else if(stat.equals(2)){
-            personViewHolder.status.setBackgroundResource(R.color.colorYellow);
+            ViewHolder.status.setBackgroundResource(R.color.colorYellow);
         }else{
-            personViewHolder.status.setBackgroundResource(R.color.colorRed);
+            ViewHolder.status.setBackgroundResource(R.color.colorRed);
         }
-
-
-
-        personViewHolder.deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which){
-                            case DialogInterface.BUTTON_POSITIVE:
-                                //Yes button clicked
-                                hold h = holds.get(i);
-                                deleteHold(h);
-                                holds.remove(h);
-                                notifyDataSetChanged();
-                                break;
-
-                            case DialogInterface.BUTTON_NEGATIVE:
-                                //No button clicked
-                                break;
-                        }
-                    }
-                };
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(c);
-                builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
-                        .setNegativeButton("No", dialogClickListener).show();
-
-
-            }
-        });
-
-
-
-
     }
 
-
-    public void deleteHold(hold h){
-        mHoldReference.child(h.getItemName()).removeValue();
-    }
-
-/*
-    @Override
-    public void onBindViewHolder(CoursesViewHolder holder, int position) {
-        Player player = mArrayCourses.get(position);
-        holder.name.setText(player.getName());
-        holder.counter.setText(String.valueOf(player.getCount()));
-        holder.voteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Do your work here
-            }
-        });
-
-    }*/
+    TextView name;
+    TextView quantity;
+    TextView item;
+    TextView status;
 
     @Override
     public void onAttachedToRecyclerView(RecyclerView recyclerView) {
         super.onAttachedToRecyclerView(recyclerView);
     }
 
+    @Override
+    public int getItemCount() {
+        return userHolds.size();
+    }
 
 }
--- app/src/main/java/jackal/org/adminandroid/userCreatedHolds.java
+++ app/src/main/java/jackal/org/adminandroid/userCreatedHolds.java
@@ -1,21 +1,17 @@
 package jackal.org.adminandroid;
 
-import android.app.Fragment;
 import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
+import android.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,28 +21,112 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link userCreatedHolds.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link userCreatedHolds#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class userCreatedHolds extends Fragment {
 
-        TextView mUserFullName, mUserEmail;
-        List<hold> userHolds;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    public List<hold> userHolds;
+    private OnFragmentInteractionListener mListener;
 
-        private RecyclerView mRecyclerView;
-        private RecyclerView.Adapter mAdapter;
-        private RecyclerView.LayoutManager mLayoutManager;
-
-        public userCreatedHolds(){
-
-        }
-
-    //database
-    FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
     DatabaseReference mHoldReference;
+    FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
 
 
-        ValueEventListener eventListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+    public userCreatedHolds() {
+
+    }
+
+    static userCreatedHolds newInstance(String param1, String param2) {
+        userCreatedHolds fragment = new userCreatedHolds();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mHoldReference = mFirebaseDatabase.getReference("holds");
+        mHoldReference.addListenerForSingleValueEvent(eventListener);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_user_created_holds, container, false);
+        mRecyclerView = rootView.findViewById(R.id.sectionHolds);
+
+        initHolds();
+
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new RVAdapter(userHolds);
+        mRecyclerView.setAdapter(mAdapter);
+
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    public void initHolds(){
+         userHolds = new ArrayList<>();
+
+       // userHolds.add(new hold("j","j","j","j","j",0));
+       // userHolds.add(new hold("jg","jggg","jg","j","j",0));
+       // userHolds.add(new hold("jgg","jgggg","jgg","j","j",0));
+       // userHolds.add(new hold("jgg","jggg","jggg","j","j",0));
+
+    }
+
+
+
+    ValueEventListener eventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            for (DataSnapshot Snapshot: dataSnapshot.getChildren()) {
+                for(DataSnapshot postSnapshot: Snapshot.getChildren()) {
                     hold h = new hold();
                     h.setApproved(postSnapshot.child("approved").getValue(Integer.class));
                     h.setItemName(postSnapshot.child("itemName").getValue(String.class));
@@ -57,93 +137,11 @@ public class userCreatedHolds extends Fragment {
                     mAdapter.notifyDataSetChanged();
                 }
             }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {}
-        };
-
-        //String name, email, address, phoneNumber, key;
-
-        private OnFragmentInteractionListener mListener;
-
-
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            mHoldReference = mFirebaseDatabase.getReference("holds");
-            mHoldReference.addListenerForSingleValueEvent(eventListener);
-        }
-
-
-
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_user_created_holds, container, false);
-            mRecyclerView = rootView.findViewById(R.id.sectionHolds);
-            mUserFullName = rootView.findViewById(R.id.name);
-            mUserEmail = rootView.findViewById(R.id.email);
-            initHolds();
-
-            mRecyclerView.setHasFixedSize(true);
-            mLayoutManager = new LinearLayoutManager(getActivity());
-            mRecyclerView.setLayoutManager(mLayoutManager);
-            mAdapter = new RVAdapter(userHolds, mHoldReference, getActivity());
-            mRecyclerView.setAdapter(mAdapter);
-
-            mAdapter.notifyDataSetChanged();
-            return rootView;
-
-
         }
 
         @Override
-        public void onStop() {
-            super.onStop();
-        }
-        @Override
-        public void onPause() {
-            super.onPause();
-        }
-        @Override
-        public void onResume() {
-
-            super.onResume();
-        }
-
-        public interface OnFragmentInteractionListener {
-            // TODO: Update argument type and name
-            void onFragmentInteraction(Uri uri);
-        }
-
-        public void submit(){
-
-        }
-
-// Attach a listener to read the data at our posts reference
+        public void onCancelled(DatabaseError databaseError) {}
+    };
 
 
-        public void initHolds(){
-            userHolds = new ArrayList<>();
-            hold tmp= new hold("j","j","j","j","j",0);
-            userHolds.add(tmp);
-            if(userHolds.isEmpty()){
-
-            }else{
-                for(hold h:userHolds){
-                    addHold(h,h.getItemName());
-                }
-            }
-
-
-        }
-
-        public void addHold(final hold Hold, String key) {
-            final DatabaseReference itemLocation = mHoldReference.push();
-            mHoldReference.child(key).setValue(Hold);
-
-        }
-
-    }
+}
